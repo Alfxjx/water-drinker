e0a5482a96950d6d17bae7f857bc07a9a3ab86cd
xujx <xjxtju@163.com>
2022-02-24 16:05:52 +0800

    fix: unuse path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-	basePath: process.env.NODE_ENV === "development" ? "" : "/water-drinker",
 	webpack: (config, options) => {
 		config.module.rules.push({
 			test: /\.svg$/,
